53fecb68fb076ce095a41c1aba7065e424e03709
John Evan <darthevanjay@gmail.com>
2017-08-15 19:32:31 +0800

    merge-correction

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,14 +2,13 @@ $btn = $("#btnSubmit");
 
 
 
-<<<<<<< HEAD
 $btn.submit(function (event) {
     event.preventDefault();
-=======
+});
 $btn.click(function (event) {
     event.preventDefault();
     console.log(event);
->>>>>>> 6a82e7347bfe48d74ecfce1e6a1fca88af35326d
+
     alert("Hey!");
 
 });
